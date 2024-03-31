4900f0aebdaed19bdc6a25d8d39f6d53b884e2a8
dev-nichola <kerjasamakol@gmail.com>
2024-03-31 16:31:48 +0700

    race condition

--- goroutine_test.go
+++ goroutine_test.go
@@ -129,17 +129,52 @@ func TestSelectMultipleChannel(t *testing.T) {
 	go GiveMeResponse(channel1)
 	go GiveMeResponse(channel2)
 
-	select {
-	case data := <-channel1:
-		fmt.Println("Data dari channel 1", data)
-	case data := <-channel2:
-		fmt.Println("Data dari channel 2", data)
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
 	}
 
-	select {
-	case data := <-channel1:
-		fmt.Println("Data dari channel 1", data)
-	case data := <-channel2:
-		fmt.Println("Data dari channel 2", data)
+}
+
+func TestDefaultChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			break
+		}
 	}
+
 }
